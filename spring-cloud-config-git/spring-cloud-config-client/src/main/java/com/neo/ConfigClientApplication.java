4f19b6f875b2632c507c7ae608b0ff5bfae57ab4
HoldDie <holddie@163.com>
2017-11-30 21:29:04 +0800

    一激动把自己代码粘进去了

--- spring-cloud-config-git/spring-cloud-config-client/src/main/java/com/neo/ConfigClientApplication.java
+++ spring-cloud-config-git/spring-cloud-config-client/src/main/java/com/neo/ConfigClientApplication.java
@@ -5,12 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-
 @SpringBootApplication
 @EnableConfigurationProperties
-public class ClientApplication {
+public class ConfigClientApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ClientApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientApplication.class, args);
+	}
 }
