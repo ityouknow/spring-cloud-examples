49bf25f8126c84dd5606d937b1dff6b99b96551f
纯洁的微笑 <ityouknow@126.com>
2018-01-22 16:52:52 +0800

    add
    分布式链路跟踪

---
+++ spring-cloud-sleuth-zipkin/spring-cloud-zuul/src/main/java/com/neo/ZuulApplication.java
@@ -0,0 +1,19 @@
+package com.neo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
+public class ZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulApplication.class, args);
+	}
+
+}
---
+++ spring-cloud-sleuth-zipkin/zipkin-server/src/main/java/com/neo/ZipkinApplication.java
@@ -0,0 +1,17 @@
+package com.neo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import zipkin.server.EnableZipkinServer;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZipkinServer
+public class ZipkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipkinApplication.class, args);
+	}
+
+}
