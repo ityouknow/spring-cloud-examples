976a37b11cb21b3410456c51fb7f35d350acf968
纯洁的微笑 <ityouknow@126.com>
2018-12-19 09:36:40 +0800

    ad

---
+++ spring-cloud-gateway/cloud-gateway/src/main/java/com/neo/GateWayApplication.java
@@ -0,0 +1,24 @@
+package com.neo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GateWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GateWayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route("path_route", r -> r.path("/about")
+						.uri("http://ityouknow.com"))
+				.build();
+	}
+
+}
